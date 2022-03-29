f98ecdef379e004936d5162482d197569a80f0c5
Sandeep Yadav <sandeepuim10@gmail.com>
2022-03-29 13:39:40 +0530

    CHECKPOINT

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,135 +1,142 @@
-import {
-  MenuItem,
-  Checkbox,
-  ListItemText,
-  Card,
-  CardContent,
-  ImageList,
-  ImageListItem,
-  ImageListItemBar,
-  FormControl,
-  Select,
-  Typography,
-  InputLabel,
-  Input,
-  Button,
-  TextField,
-} from "@material-ui/core";
 import React from "react";
-
-import Header from "../../common/header/Header";
-import moviesData from "../../common/header/moviesData";
 import "./Home.css";
-import { useNavigate } from "react-router-dom";
-const Home = (props) => {
-  const history = useNavigate();
-  const movieClickHandler = (id) => {
-    history("Detail" + id);
+import Header from "./../../common/header/Header";
+import moviesData from "./../../common/moviesData";
+import UpComingMovies from "./HomeComponents/UpcomingMovies";
+import AllMovies from "./HomeComponents/AllMovies";
+import MoviesFilterForm from "./HomeComponents/MoviesFilterForm";
+import genres from "./../../common/genre";
+import artists from "./../../common/artists";
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: moviesData,
+      formValues: {
+        movieName: "",
+        genresList: [],
+        artistsList: [],
+        releaseDateStart: null,
+        releaseDateEnd: null,
+      },
+    };
+  }
+  filterList = () => {
+    let finalFilteredMovieList = moviesData;
+    const formValues = { ...this.state.formValues };
+    if (formValues.movieName) {
+      finalFilteredMovieList = finalFilteredMovieList.filter(
+        movie =>
+          movie.title.toLowerCase() ===
+          this.state.formValues.movieName.toLowerCase()
+      );
+    }
+    if (formValues.genresList.length > 0) {
+      finalFilteredMovieList = finalFilteredMovieList.filter(movie => {
+        for (let i = 0; i < formValues.genresList.length; i++) {
+          console.log(movie.genres, formValues.genresList);
+          if (movie.genres.includes(formValues.genresList[i].name)) return true;
+        }
+        return false;
+      });
+    }
+    if (formValues.artistsList.length > 0) {
+      finalFilteredMovieList = finalFilteredMovieList.filter(movie => {
+        const fullNameArray = [];
+        movie.artists.forEach(artist =>
+          fullNameArray.push(`${artist.first_name} ${artist.last_name}`)
+        );
+        for (let i = 0; i < formValues.artistsList.length; i++) {
+          if (
+            fullNameArray.includes(
+              `${formValues.artistsList[i].first_name} ${formValues.artistsList[i].last_name}`
+            )
+          )
+            return true;
+        }
+        return false;
+      });
+    }
+    if (formValues.releaseDateStart && formValues.releaseDateEnd) {
+      const releaseDateStart = new Date(formValues.releaseDateStart);
+      const releaseDateEnd = new Date(formValues.releaseDateEnd);
+      finalFilteredMovieList = finalFilteredMovieList.filter(movie => {
+        const movieReleaseDate = new Date(movie.release_date);
+        return (
+          movieReleaseDate >= releaseDateStart &&
+          movieReleaseDate <= releaseDateEnd
+        );
+      });
+    } else if (formValues.releaseDateStart && !formValues.releaseDateEnd) {
+      const releaseDateStart = new Date(formValues.releaseDateStart);
+      finalFilteredMovieList = finalFilteredMovieList.filter(movie => {
+        const movieReleaseDate = new Date(movie.release_date);
+        return movieReleaseDate >= releaseDateStart;
+      });
+    } else if (!formValues.releaseDateStart && formValues.releaseDateEnd) {
+      const releaseDateEnd = new Date(formValues.releaseDateEnd);
+      finalFilteredMovieList = finalFilteredMovieList.filter(movie => {
+        const movieReleaseDate = new Date(movie.release_date);
+        return movieReleaseDate <= releaseDateEnd;
+      });
+    }
+    console.log(formValues.releaseDateStart, formValues.releaseDateEnd);
+    console.log(finalFilteredMovieList);
+    this.setState({ movies: finalFilteredMovieList });
   };
 
-  return (
-    <div className="main">
-      <Header />
-      <div className="upcomming-Movies">
-        <span>Upcoming Movies</span>
-      </div>
-      <div className="container">
-        <ImageList cols={5} className="imageItem">
-          {moviesData.map((movie) => (
-            <ImageListItem className="listItem" key={movie.id}>
-              <img src={movie.poster_url} style={{ height: "auto" }} />
-              <ImageListItemBar title={movie.title} />
-            </ImageListItem>
-          ))}
-        </ImageList>
-      </div>
-      <div className="flex-container">
-        <div className="left">
-          <ImageList className="movieImage" cols={3} rowHeight={400}>
-            {moviesData.map((movie, index) => (
-              <ImageListItem
-                style={{ width: "300px" }}
-                onClick={() => {
-                  movieClickHandler(movie.id);
-                }}
-              >
-                <img src={movie.poster_url} alt={movie.title} />
-                <ImageListItemBar
-                  title={movie.title}
-                  subtitle={
-                    <span>
-                      Release Date:{" "}
-                      {new Date(movie.release_date).toDateString()}
-                    </span>
-                  }
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-        </div>
+  handleSubmit = e => {
+    e.preventDefault();
+    this.filterList();
+    console.log("Submitted");
+  };
 
-        <div className="right">
-          <Card>
-            <CardContent>
-              <FormControl className="formControl">
-                <Typography
-                  color="primary"
-                  style={{ textAlign: "center", color: "theme.spacing.unit" }}
-                >
-                  Find Movies By:
-                </Typography>
-              </FormControl>{" "}
-              <br />
-              <FormControl className="formControl">
-                <InputLabel htmlFor="movieName">Movie Name</InputLabel>
-                <Input id="movieName" />
-              </FormControl>{" "}
-              <br />
-              <FormControl className="formControl">
-                <InputLabel htmlFor="genreList">Genre</InputLabel>
-                <Select multiple input={<Input id="genreList" />}>
-                  ListofData
-                </Select>
-              </FormControl>
-              <br />
-              <FormControl className="formControl">
-                <InputLabel htmlFor="artistList">Artist</InputLabel>
-                <Select multiple input={<Input id="artistList" />}>
-                  ArtistData
-                </Select>
-              </FormControl>{" "}
-              <br />
-              <FormControl className="formControl">
-                <TextField
-                  id="releaseDateStart"
-                  label="Release Date Start"
-                  type="date"
-                  defaultValue=""
-                  InputLabelProps={{ shrink: true }}
-                />
-              </FormControl> <br />
-              <FormControl className="formControl">
-                <TextField
-                  id="releaseDateEnd"
-                  label="Release Date End"
-                  type="date"
-                  defaultValue=""
-                  InputLabelProps={{ shrink: true }}
-                />
-              </FormControl>
-              <br />
-              <br />
-              <FormControl className="formControl">
-                <Button variant="contained" color="primary">
-                  APPLY
-                </Button>
-              </FormControl>
-            </CardContent>
-          </Card>
+  handleChange = e => {
+    const formValues = { ...this.state.formValues };
+    formValues[e.target.name] = e.target.value;
+    this.setState({ formValues });
+  };
+
+  handleAutoCompleteChange = (e, v) => {
+    const formValues = { ...this.state.formValues };
+    formValues[`${e.target.id.split("-")[0]}List`] = v;
+    this.setState({ formValues });
+  };
+
+  handleDateChange = (d, v, name) => {
+    const formValues = { ...this.state.formValues };
+    formValues[name] = new Date(d).toDateString();
+    this.setState({ formValues });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="upcoming-movies-header">
+        <span>Upcoming Movies</span>
+        </div>
+        <UpComingMovies movies={moviesData} />
+        <div className="flex-container">
+          <div className="left">
+            <AllMovies movies={this.state.movies} />
+          </div>
+          <div className="right">
+            <MoviesFilterForm
+              genres={genres}
+              artists={artists}
+              handleSubmit={this.handleSubmit}
+              handleChange={this.handleChange}
+              handleAutoCompleteChange={this.handleAutoCompleteChange}
+              handleDateChange={this.handleDateChange}
+              releaseDateStart={this.state.formValues.releaseDateStart}
+              releaseDateEnd={this.state.formValues.releaseDateEnd}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Home;
